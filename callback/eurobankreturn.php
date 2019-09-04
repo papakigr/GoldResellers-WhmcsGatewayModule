3d06b3b5658910e270ce5c499715b3daae342596
Voula Troulaki <v.trulaki@usableweb.gr>
2019-09-04 14:38:03 +0300

    delete old versions , add version 4 for 3d secure changes

---
+++ callback/eurobankreturn.php
@@ -0,0 +1,73 @@
+<?php
+
+require_once __DIR__ . '/../../../init.php';
+
+$whmcs->load_function('gateway');
+$whmcs->load_function('invoice');
+
+
+$gatewaymodule = "eurobanklib"; # Enter your gateway module name here replacing template
+$GATEWAY = getGatewayVariables($gatewaymodule);
+if (!$GATEWAY['type']) {
+    die("Module Not Activated");
+}
+
+$mid = $_POST['mid'];
+$orderid = $_POST['orderid'];
+$orderAmount = $_POST['orderAmount'];
+$status = $_POST['status'];
+$post_digest = $_POST['digest'];
+$paymentRef = isset($_POST['paymentRef']) ? $_POST['paymentRef'] : '';
+$txId = $_POST['txId'];
+$riskScore = isset($_POST['riskScore']) ? $_POST['riskScore'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
+$payMethod = isset($_POST['payMethod']) ? $_POST['payMethod'] : '';
+$invoiceId = $orderid;
+$form_data = '';
+foreach ($_POST as $k => $v) {
+    if (!in_array($k, array('_charset_', 'digest', 'submitButton'))) {
+        $form_data .= $_POST[$k];
+    }
+}
+$form_data .= $GATEWAY['gatewaymerchantpassword'];
+$paymentSuccess = false;
+$digest = calculate_digest($form_data);
+if ($digest != $post_digest) {
+    logTransaction($GATEWAY["name"], $_POST,
+        "Unsuccessful Confirmation. Invalid Digest."); # Save to Gateway Log: name, data array, status
+} else {
+    //AUTHORIZED, CAPTURED,CANCELED,REFUSED,ERROR
+    if (isset($_GET['result']) && ($_GET['result'] == 'success')) {
+
+        if ($status == 'CAPTURED') {
+            $Ref = $txId;
+            $Amount = $orderAmount;
+            $invoiceId = checkCbInvoiceID($invoiceId,
+                $GATEWAY["name"]); # Checks invoice ID is a valid invoice number or ends processing
+            checkCbTransID($Ref); # Checks transaction number isn't already in the database and ends processing if it does            
+            logTransaction($GATEWAY["name"], $_POST, "Successful"); # Save to Gateway Log: name, data array, status
+            addInvoicePayment($invoiceId, $Ref, $Amount, 0,
+                $gatewaymodule); # Apply Payment to Invoice: invoiceid, transactionid, amount paid, fees, modulename
+            $paymentSuccess = true;
+        } else {
+            if ($status == 'ERROR') {
+                logTransaction($GATEWAY["name"], $_POST,
+                    "Unsuccessful Confirmation. Unknown error."); # Save to Gateway Log: name, data array, status
+            } else {
+                if ($status == 'CANCELED') {
+                    logTransaction($GATEWAY["name"], $_POST,
+                        "Unsuccessful Confirmation. CANCELED"); # Save to Gateway Log: name, data array, status
+                } else {//Failed Response codes
+                    logTransaction($GATEWAY["name"], $_POST,
+                        "Unsuccessful Confirmatio. $status"); # Save to Gateway Log: name, data array, status
+                }
+            }
+        }
+    } else {
+        if (isset($_GET['result']) && ($_GET['result'] == 'failure')) {
+            logTransaction($GATEWAY["name"], $_POST,
+                "Unsuccessful Confirmation"); # Save to Gateway Log: name, data array, status
+        }
+    }
+}
+callback3DSecureRedirect($invoiceId, $paymentSuccess);
--- eurobanklib.php
+++ eurobanklib.php
@@ -15,12 +15,14 @@ $GATEWAYMODULE = array(
 function eurobanklib_activate()
 {
     # Here you need to define the fields required to configure your gateway - vars: template, type, name, default value, friendly name, size, description
-    
+
     defineGatewayField("eurobanklib", "text", "gatewaymerchantid", "", "MerchantId", "20", "");
     defineGatewayField("eurobanklib", "text", "gatewaymerchantpassword", "", "Shared Secret", "20", "");
-    defineGatewayField("eurobanklib", "text", "gatewaycurrency", "", "Currency", "20", "ISO 4217 alphabetic code (EUR, USD)");
+    defineGatewayField("eurobanklib", "text", "gatewaycurrency", "", "Currency", "20",
+        "ISO 4217 alphabetic code (EUR, USD)");
     defineGatewayField("eurobanklib", "text", "gatewaylang", "", "Language", "20", "Type GR");
-    defineGatewayField("eurobanklib", "yesno", "testmode", "", "Test Mode", "", "Enable testing mode. CAUTION: DO NOT USE IN PRODUCTION!");
+    defineGatewayField("eurobanklib", "yesno", "testmode", "", "Test Mode", "",
+        "Enable testing mode. CAUTION: DO NOT USE IN PRODUCTION!");
 }
 
 function eurobanklib_link($params)
@@ -30,7 +32,7 @@ function eurobanklib_link($params)
     //$gatewayusername = $params['username'];
     //$gatewaytestmode = $params['testmode'];
     //$url"https://vpos.eurocommerce.gr/vpos/shophandlermpi";
-    
+
     # Invoice Variables
     $invoiceid = $params['invoiceid'];
     $description = $params["description"];
@@ -41,8 +43,8 @@ function eurobanklib_link($params)
 
     $url = 'https://vpos.eurocommerce.gr/vpos/shophandlermpi';
     if ($params['testmode'] == "on") {
-		$url = 'https://euro.test.modirum.com/vpos/shophandlermpi';
-	}
+        $url = 'https://euro.test.modirum.com/vpos/shophandlermpi';
+    }
     # Client Variables
     $firstname = $params['clientdetails']['firstname'];
     $lastname = $params['clientdetails']['lastname'];
@@ -58,32 +60,45 @@ function eurobanklib_link($params)
     # System Variables
     $companyname = $params['companyname'];
     $systemurl = $params['systemurl'];
-     
+
 
     # Enter your code submit to the gateway...
-    $gatewaymerchantid = $params['gatewaymerchantid'];    
-    $price=$amount;
-    $reference=$invoiceid." ".date("his");
-    
+    $gatewaymerchantid = $params['gatewaymerchantid'];
+    $price = $amount;
+    $reference = $invoiceid . " " . date("his");
 
-    $form_data_array = array('mid' => $gatewaymerchantid, 'lang' => $language, 'deviceCategory' => '0', 'orderid' => $invoiceid, 'orderDesc' => 'Order #' . $invoiceid, 'orderAmount' => $price, 'currency' => $currency, 'payerEmail' => $email, 'confirmUrl' => $systemurl . "modules/gateways/callback/eurobankreturn.php?result=success", 'cancelUrl' => $systemurl . "modules/gateways/callback/eurobankreturn.php?result=failure",);
+
+    $form_data_array = array(
+        'mid' => $gatewaymerchantid,
+        'lang' => $language,
+        'deviceCategory' => '0',
+        'orderid' => $invoiceid,
+        'orderDesc' => 'Order #' . $invoiceid,
+        'orderAmount' => $price,
+        'currency' => $currency,
+        'payerEmail' => $email,
+        'confirmUrl' => $systemurl . "modules/gateways/callback/eurobankreturn.php?result=success",
+        'cancelUrl' => $systemurl . "modules/gateways/callback/eurobankreturn.php?result=failure",
+    );
     $form_secret = $params['gatewaymerchantpassword'];
     $form_data = utf8_encode(implode("", $form_data_array)) . $form_secret;
 
-    $digest =calculate_digest($form_data);
+    $digest = calculate_digest($form_data);
 
-    $html = '<form name="PayformVisa" action="'.$url.'" method="post" id="eb_payment_form" target="_top" accept-charset="UTF-8">';
+    $html = '<form name="PayformVisa" action="' . $url . '" method="post" id="eb_payment_form" target="_top" accept-charset="UTF-8">';
     foreach ($form_data_array as $key => $value) {
         $html .= '<input type="hidden" id="' . $key . '" name="' . $key . '" value="' . utf8_encode($value) . '"/>';
     }
     $html .= '<input type="hidden" id="digest" name="digest" value="' . ($digest) . '"/>';
     $html .= '<input type="submit" value="Pay"></form>';
-    $code=$html;
+    $code = $html;
 
     return $code;
 }
+
 function calculate_digest($input)
 {
     $digest = base64_encode(sha1(($input), true));
+
     return $digest;
 }
